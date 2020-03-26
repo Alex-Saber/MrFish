7cdd52ca7062ccd3aa30f570ec0a1e6b604f768d
Alex Saber <alex.saber@dascena.com>
2020-03-26 01:05:15 -0700

    Adds attack animation

--- Assets/Scripts/player_controller.cs
+++ Assets/Scripts/player_controller.cs
@@ -5,55 +5,126 @@ using UnityEngine;
 public class player_controller : MonoBehaviour {
 
 	public Animator animator;
-	public int direction;
-	public float running_speed = 0.5f;
-	public float walking_speed = 0.05f;
+	public Camera cam;
+
+	private int combatTime = 300;
+	private int inCombatTimer = 0;
+
+	private float running_speed = 0.7f;
+	private float walking_speed = 0.3f;
+	private float curr_speed;
+
+	private int direction;
+	private bool moving;
+	private bool running;
+	private bool attacking;
+	private bool inCombat;
+
 	// Use this for initialization
 	void Start () {
 		direction = 1;
+		moving = false;
+		curr_speed = walking_speed;
 	}
 
-	// Update is called once per frame
+	// Update is called once per frame. This is the "main" function.
 	void Update () {
 
-		float Horizontal = Input.GetAxis ("Horizontal");
+		// Gathers the user inputs and assigns the appropriate state variables.
+		int newDirection = handleInputs ();
 
-		bool [] state = handleAnimations (Horizontal);
-		handleFlipCharacter (Horizontal);
+		// Handles the player's inCombat status (timer that counts down)
+		handleCombatTimer();
 
-		// Compute current speed
+		// Handles which animations play as well as animation parameters.
+		handleAnimations ();
+
+		// Handle which direction the character is facing.
+		handleCharacterDirection (newDirection);
+
+		// Handle player movement.
+		handleMovement ();
+
+		print (Input.mousePosition + " " + transform.position);
+	}
+
+	int handleInputs() {
+		
+		int newDirection = direction;
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+			newDirection = -1;
+			moving = true;
+		} else if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+			newDirection = 1;
+			moving = true;
+		} else {
+			moving = false;
+		}
+
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+			running = true;
+		} else {
+			running = false;
+		}
+
+		if (Input.GetMouseButton (0)) {
+			attacking = true;
+			// Find location of mouse to determine which direction to attack in.
+			Vector3 mouseLoc = cam.ScreenToWorldPoint(Input.mousePosition);
+			if (mouseLoc.x >= transform.position.x) {
+				newDirection = 1;
+			} else {
+				newDirection = -1;
+			}
+
+			print (newDirection);
+		} else {
+			attacking = false;
+		}
+
+		return newDirection;
+	}
+
+	void handleCombatTimer() {
+
+		if (isAttacking ()) {
+			inCombatTimer = combatTime;
+		}
+			
+		if (inCombatTimer > 0) {
+			inCombat = true;
+			inCombatTimer -= 1;
+		} else {
+			inCombat = false;
+		}
+	}
+
+	void handleMovement() {
 		float curr_speed = 0.0f;
-		if (Mathf.Abs(Horizontal) > 0.1) {
-			if (state [0] && state [1]) {
+		if (moving) {
+			// If moving and running set speed to running_speed
+			if (moving && running) {
 				curr_speed = running_speed;
-			} else if (state [0] && !state [1]) {
+			} 
+			// If moving but not running set speed to walking speed
+			else if (moving && !running) {
 				curr_speed = walking_speed;
 			}
 		}
-
-		handleMovement (curr_speed);
-
+			
+		if (!isAttacking()) {
+			transform.position = new Vector3 (
+				transform.position.x + curr_speed * direction, 
+				transform.position.y,
+				transform.position.z
+			);
+		}
 	}
 
-	void handleMovement(float speed) {
-		transform.position = new Vector3(
-			transform.position.x + speed * direction, 
-			transform.position.y,
-			transform.position.z
-		);
-	}
-
-	void handleFlipCharacter(float horizontal) {
-		if (horizontal > 0.0) {
-			if (direction == -1) {
-				flipCharacter ();
-				direction = 1;
-			}
-		} else if (horizontal < 0.0) {
-			if (direction == 1) {
-				flipCharacter ();
-				direction = -1;
-			}
+	void handleCharacterDirection(int newDirection) {
+		if (direction != newDirection) {
+			flipCharacter ();
+			direction = newDirection;
 		}
 	}
 
@@ -65,26 +136,21 @@ public class player_controller : MonoBehaviour {
 		);
 	}
 
-	bool [] handleAnimations(float horizontal) {
+	void handleAnimations() {
 
-		bool run = false;
-		bool walk = false;
-
-		if (horizontal != 0) {
-			walk = true;
+		if (attacking) {
+			animator.Play ("side_slash_01");
 		}
-
-		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
-			run = true;
-		}
-
-		animator.SetBool ("moving", walk);
-		animator.SetBool ("running", run);
-
-		bool[] state = new bool[2];
-		state [0] = walk;
-		state [1] = run;
-		return state;
+			
+		animator.SetBool ("moving", moving);
+		animator.SetBool ("running", running);
+		animator.SetBool ("inCombat", inCombat);
 	}
+
+
+	bool isAttacking() {
+		return animator.GetCurrentAnimatorStateInfo (0).IsName ("side_slash_01") || animator.GetCurrentAnimatorStateInfo (0).IsName ("side_slash_02");
+	}
+
 }
 
